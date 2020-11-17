044d3f288d1b3e1b276698efbf914b4022bed9e0
Matthias Marczyszyn <m.marczyszyn@gmail.com>
2020-11-18 00:35:16 +0100

    Zadania 1,4,5,6,7

---
+++ Lab2/skrypt5i6.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#segment uzywany tylko w zad5
+#trap "echo SIGINT" INT
+#trap "echo SIGQUIT" SIGQUIT
+#trap "echo SIGFPE" SIGFPE
+#trap "echo SIGILL" SIGILL
+#trap "echo SIKILL" SIGKILL
+############################################
+
+ while [ 1 -eq 1 ]; do
+TZ=':Poland/Warsaw' date
+sleep 10s
+ done
