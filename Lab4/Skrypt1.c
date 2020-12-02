6a7715b12a83bd45c9ebbdb719dbabd9342971c5
Matthias Marczyszyn <m.marczyszyn@gmail.com>
2020-12-02 19:26:53 +0100

    zadanie 1 skrypt

---
+++ Lab4/Skrypt1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#define Buf_rozmiar 32
+int main(int argc, char *argv[]) {
+
+	pid_t pid;
+	int fd[2], in_fd, n;
+	char buf [Buf_rozmiar];
+
+	if(pipe(fd)<0)//sprawdzanie pipe
+        {
+        fprintf(stderr, "blad\n");
+        }
+
+	pid = fork();//tworzenie dziecka
+
+	if(pid==0)//dziecko
+		{
+		close(fd[1]);
+		while(n = read(fd[0],&buf,Buf_rozmiar)>0)
+            {
+                printf("#%s#\n",buf);
+            }
+            close(fd[0]);
+		}
+
+	else if(pid>0)//rodzic
+	{
+		close(fd[0]);
+		//scanf("%s",fname);
+		if((in_fd=open(argv[1],O_RDONLY))<0)
+			{
+			fprintf(stderr,"eror\n");
+			return 2;
+			}
+
+	while ((n= read(in_fd, &buf,Buf_rozmiar))>0){
+		if(write(fd[1],&buf,n)<0){
+			fprintf(stderr,"eror\n");
+			return 3;
+			}
+
+		}
+	}
+}
